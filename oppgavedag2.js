127b09656d3b7c4a6bab36e9a2033097d6a7b1be
sander1222 <sander122.kodehode@gmail.com>
2024-10-22 14:29:27 +0200

    Add files via upload

---
+++ oppgavedag2.js
@@ -0,0 +1,65 @@
+// oppgave 1
+
+// man kan kopiere ut en linje med kode ved å markere den linjen også trykke "ctrl + *" tastene, du kan komentere ut flere ved og bare markere de linjene du ønsker å komentere ut
+
+//oppgave 2
+
+// du bruker <script> src=""</script> og bruker navnet til javascript navnet ditt mellom "" som for eks "javascript.js"
+
+//oppgave 3
+
+let x = 10;
+let y = 10;
+
+// en operator er Addition(+) og den plusser ting sammen som for eks
+// console.log(x + y);
+
+// en annen operator er Subtraction(-) som minuser som for eks
+console.log(x - y);
+
+// Multiplication er også en operator som ganger sammen som for eks
+console.log(x * y);
+
+//Division er en annen ofte brukt opperator som deler to tall med hverandre som for eks
+console.log(x / y);
+
+//oppgave 4
+//objekter er variabler men de kan også holde flere verdier
+//som i oppgave 5 er ansatt1 et objekt med flere verider i seg som for eks "firstName og lastName"
+
+//oppgave 5
+
+const ansatt1 = {
+  firstName: "Reidun",
+  lastName: "baksletten",
+  adresse: "grøftkantten 4",
+};
+
+console.log(ansatt1.firstName + " " + ansatt1.lastName + " " + ansatt1.adresse);
+
+const ansatt2 = {
+  firstName: "Mike",
+  middleName: "Row",
+  lastName: "Pness",
+  adresse: "snobby shores12",
+};
+
+console.log(
+  ansatt2.firstName +
+    " " +
+    ansatt2.middleName +
+    " " +
+    ansatt2.lastName +
+    " " +
+    ansatt2.adresse
+);
+
+const ansatt3 = {
+  firstName: "Oliver",
+  lastName: "Twist",
+  adresse: "666 Hell's Kitchen Drive",
+};
+
+console.log(
+  ansatt3S.firstName + " " + ansatt3.lastName + " " + ansatt3.adresse
+);
